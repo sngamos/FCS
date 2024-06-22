6eecc5aefad51efff3fa5eb681697ed7556364cd
Codemos <sngamos9@gmail.com>
2024-06-22 21:26:44 +0800

    done with ecb

--- Lab4/ecb.py
+++ Lab4/ecb.py
@@ -8,8 +8,28 @@ nokeybits=80
 blocksize=64
 
 
-def ecb(infile,outfile,key,mode):
-    pass
+def ecb(infile, outfile, key, mode):
+    with open(infile, 'rb') as content:
+        with open(outfile, 'wb') as content_out:
+            while True:
+                block = content.read(blocksize // 8)  # Read 8 bytes (64 bits)
+                if len(block) == 0:
+                    print("{modes} completed".format(modes = mode))
+                    break
+                elif len(block) < blocksize // 8:
+                    # Padding the last block if it's less than 64 bits
+                    block += b'\x00' * (blocksize // 8 - len(block))
+                
+                block_int = int.from_bytes(block, byteorder='big')
+                
+                if mode == 'encrypt':
+                    processed_block = present(block_int, key)
+                elif mode == 'decrypt':
+                    processed_block = present_inv(block_int, key)
+                else:
+                    raise ValueError("Mode should be either 'encrypt' or 'decrypt'")
+                
+                content_out.write(processed_block.to_bytes(blocksize // 8, byteorder='big'))
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description='Block cipher using ECB mode.')
@@ -22,6 +42,10 @@ if __name__=="__main__":
     infile=args.infile
     outfile=args.outfile
     keyfile=args.keyfile
+    mode=args.mode
 
+    with open(keyfile, 'r') as kf:
+        key = int(kf.read(), 16)  # Read the key as a hexadecimal string and convert to integer
 
+    ecb(infile, outfile, key, mode)
 
