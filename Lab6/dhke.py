4d2b817ef63ae574c6dcea1f28adbda5b99c55be
Openwide <sngamos9@gmail.com>
2024-07-11 15:45:13 +0800

    unfinised lab6

--- Lab6/dhke.py
+++ Lab6/dhke.py
@@ -1,24 +1,36 @@
 # 50.042 FCS Lab 6 template
 # Year 2021
 
-import primes
+from primes import *
 import random
+from present import present, present_inv
+
 
 
 def dhke_setup(nb):
-    pass
+    p = 1208925819614629174706189
+    a = 2
+    return p,a
+
 
 
 def gen_priv_key(p):
-    pass
+    a = random.randint(1, p-2)
+    return a
 
 
 def get_pub_key(alpha, a, p):
-    pass
+    A = square_multiply(alpha, a, p)
+    return A
 
 
 def get_shared_key(keypub, keypriv, p):
-    pass
+    sharedkey = square_multiply(keypub, keypriv, p)
+    return sharedkey
+
+
+message = "Hello, World!"
+
 
 
 if __name__ == "__main__":
@@ -42,3 +54,9 @@ if __name__ == "__main__":
     print("My shared key is: ", sharedKeyA)
     print("Test other shared key is: ", sharedKeyB)
     print("Length of key is %d bits." % sharedKeyA.bit_length())
+    message = 123
+    print("My plaintext: " , message)
+    cipher = present(bin(message)[2:], bin(sharedKeyA)[2:])
+    print("My Cipher text: ", cipher)
+    decrypt = present_inv(bin(cipher)[2:], bin(sharedKeyB)[2:])
+    print("Your decrypted message: ", decrypt)
--- Lab6/primes.py
+++ Lab6/primes.py
@@ -3,14 +3,20 @@
 
 import random
 def square_multiply(a,x,n):
-    pass
+    y =1
+    n_b = bin(x)[2:]
+    for i in range(len(n_b)):
+        y=(y**2)%n
+        if n_b[i]=='1':
+            y=(y*a)%n
+    return y
 
 def miller_rabin(n, a):
     pass
 
 def gen_prime_nbits(n):
     pass
-
+"""
 if __name__=="__main__":
     print('Is 561 a prime?')
     print(miller_rabin(561,2))
@@ -23,3 +29,5 @@ if __name__=="__main__":
     print(gen_prime_nbits(100))
     print('Random number (80 bits):')
     print(gen_prime_nbits(80))
+"""
+print(square_multiply(2,33,5))
